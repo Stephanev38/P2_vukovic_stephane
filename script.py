8c5de497b1194e2ce2406c6fbb592aa69846eef5
Vukovic <vukovic@free.fr>
2020-10-25 14:01:23 +0100

    premier script fini

--- script.py
+++ script.py
@@ -1,31 +1,28 @@
+# -*-coding:utf8 -*
+
+import os
 import csv
 import requests
 import urllib3
 from bs4 import BeautifulSoup
 
-url = "http://books.toscrape.com/catalogue/tipping-point-for-planet-earth-how-close-are-we-to-the-edge_643/index.html"
+url = "http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
 
-reponse = requests.get(url) #récupération de la page
-page = reponse.content
-soup = BeautifulSoup(page, "html.parser")
+contenu = requests.get(url) # récupération de la page à analyser
+page = BeautifulSoup(contenu.text, "html.parser")
 
 product_page_url = url
-col1 = product_page_url
-print("Product page url", url) #impression de l'url de la page
+print(url)
 
-"""trs = soup.findAll('tr')
-[print(str(tr) + '\n\n') for tr in trs]""" #impression des lignes du tableau du site
-"""print(len(trs))""" #impression du nombre de lignes
-
-table = soup.find('table', {'class': 'table table-striped'})
+table = page.find('table', {'class': 'table table-striped'})
 th = table.find('th', text='UPC')
 td = th.findNext('td')
 col2 = td
 print(th.text, '=', td.text)
 
-title = soup.find('title')
-col3 = title
-print(title.text) #impression du titre
+div = page.find('div', {'class': 'col-sm-6 product_main'})
+h1 = div.find('h1')
+print("Title :", h1.text) #impression du titre
 
 th = table.find('th', text='Price (incl. tax)')
 td = th.findNext('td')
@@ -42,9 +39,8 @@ td = th.findNext('td')
 col6 = td
 print(th.text, '=', td.text) #impression d'availability
 
-div = soup.find('div', {'id': 'product_description'})
+div = page.find('div', {'id': 'product_description'})
 p = div.findNext('p')
-col7 = p
 print("Product description : ", p.text) #impression de product description
 
 th = table.find('th', text='Product Type')
@@ -57,22 +53,12 @@ td = th.findNext('td')
 col9 = td
 print(th.text, '=', td.text) #impression du nombre dans le stock
 
-div = soup.find('div', {'class': 'item active'})
+div = page.find('div', {'class': 'item active'})
 img_tags = div.findNext('img')['src']
-print("Image url : http://books.toscrape.com" + img_tags[5:]) #impression de l'url de l'image
 col10 = "http://books.toscrape.com" + img_tags[5:]
+print("Image url : http://books.toscrape.com" + img_tags[5:]) #impression de l'url de l'image
 
-file_name = "fichier.csv"
-file = open(file_name, "w")
-writer = csv.writer(file)
-writer.writerow(("product page url", "universal product code", "title", "price including tax", "price excluding tax", "number available", "product description", "category", "review rating", "image url"))
-writer.writerow((col1 , col2.text, col3.text, col4.text, col5.text, col6.text, col7.text, col8.text, col9.text, col10))
-file.close()
-
-"""with open('fichier.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(fichieriterable)"""
-
-
-
-"""print(soup)"""
+with open('fichier.csv', 'wt') as out_file:
+    csv_writer = csv.writer(out_file, delimiter='\t')
+    csv_writer.writerow(['product page url', 'universal product code', 'title', 'price including tax', 'price excluding tax', 'number available', 'product description', 'category', 'review rating', 'image url'])
+    csv_writer.writerow([product_page_url, col2.text, h1.text, col4.text, col5.text, col6.text, p.text, col8.text, col9.text, col10])
